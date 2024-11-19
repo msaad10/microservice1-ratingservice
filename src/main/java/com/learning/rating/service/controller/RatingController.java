524d4096c20519162a34383de30ebee9cc7699dc
msaad10 <saad.muhammad@tenpearls.com>
2024-11-19 15:39:30 +0500

    Update pom.xml to add spring cloud + eureka dependency AND update application.yml to reguster with eureka server AND minor API change

--- src/main/java/com/learning/rating/service/controller/RatingController.java
+++ src/main/java/com/learning/rating/service/controller/RatingController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/rating")
+@RequestMapping("/v1/ratings")
 public class RatingController {
     private final RatingService ratingService;
 
